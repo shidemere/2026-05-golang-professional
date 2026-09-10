aa3a6d0c00e070306797fe0ef373abc01a524759
shidemere <reinofgrein@gmail.com>
2026-09-10 14:39:01 +0300

    done

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,21 +9,41 @@ import (
 	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	// ErrErrorsLimitExceeded too many errors.
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	// ErrNegativeOrZeroWorkersCount negative workers number (or zero).
+	ErrNegativeOrZeroWorkersCount = errors.New("workers number less or equal zero")
+	// ErrNegativeMaxErrorCount allowable count of errors is negative.
+	ErrNegativeMaxErrorCount = errors.New("count of errors can't be less than zero")
+)
 
+// Task represent some work for to do.
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if len(tasks) == 0 {
+		return nil
+	}
+
+	if n <= 0 {
+		log.Println("numbers of workers can't be less or equal zero")
+		return ErrNegativeOrZeroWorkersCount
+	}
+
+	if m < 0 {
+		log.Println("count of errors can't be less then zero")
+		return ErrNegativeMaxErrorCount
+	}
+
+	allTasksCount := &AllTasksCounter{all: len(tasks)}
 	var wg sync.WaitGroup
-	var err error
 	taskChan := make(chan Task)
 	coordinatorCtx, coordinatorCancelFunc := context.WithCancel(context.Background())
-	errChan := make(chan struct{})
+	successChan := make(chan bool)
 	FillChanWithTasks(coordinatorCtx, tasks, &wg, taskChan)
-	TaskErrorShutdownCoordinator(coordinatorCancelFunc, &wg, errChan, m)
-
+	hasCompletedNormally := TaskErrorShutdownCoordinator(coordinatorCancelFunc, &wg, successChan, m, allTasksCount)
 	for i := range n {
 		wg.Go(func() {
 			for {
@@ -33,32 +53,58 @@ func Run(tasks []Task, n, m int) error {
 						log.Printf("task channel closed, worker #%d shutdown\n", i)
 						return
 					}
-					ProcessTask(i, task, errChan)
+					ProcessTask(coordinatorCtx, i, task, successChan)
 				case <-coordinatorCtx.Done():
-					err = ErrErrorsLimitExceeded
-					log.Printf("task channel closed, worker #%d shutdown\n", i)
 					return
 				}
 			}
 		})
 	}
+	err := <-hasCompletedNormally
 	wg.Wait()
+	if err != nil {
+		return fmt.Errorf("reached maximum errors: %w", err)
+	}
 	return err
 }
 
 // TaskErrorShutdownCoordinator think about correct naming, because now it's bullshit.
-func TaskErrorShutdownCoordinator(cancel context.CancelFunc, wg *sync.WaitGroup, errChan chan struct{}, m int) {
+func TaskErrorShutdownCoordinator(
+	cancel context.CancelFunc,
+	wg *sync.WaitGroup,
+	successChan chan bool,
+	m int,
+	allTasks *AllTasksCounter,
+) chan error {
+	hasCompletedNormally := make(chan error)
 	wg.Go(func() {
-		count := 0
-		for {
-			<-errChan
-			count++
-			if count >= m {
+		for success := range successChan {
+			if !success {
+				// if we expect no error, but got one
+				if m == 0 {
+					log.Println("reached maximum error's count. Canceling work")
+					hasCompletedNormally <- ErrErrorsLimitExceeded
+					cancel()
+					return
+				}
+				allTasks.IncrementErrorCounter()
+			} else {
+				allTasks.IncrementSuccessCounter()
+			}
+			if allTasks.GetAllErrorsCount() == m && m != 0 {
+				log.Println("reached maximum error's count. Canceling work")
+				hasCompletedNormally <- ErrErrorsLimitExceeded
+				cancel()
+				return
+			}
+			if (allTasks.GetAllErrorsCount() + allTasks.GetAllSuccessCount()) == allTasks.GetAll() {
+				hasCompletedNormally <- nil
 				cancel()
 				return
 			}
 		}
 	})
+	return hasCompletedNormally
 }
 
 // FillChanWithTasks takes our slice of tasks and push it to the channel with non-blocking algo.
@@ -66,27 +112,59 @@ func FillChanWithTasks(coordinatorCtx context.Context, tasks []Task, wg *sync.Wa
 	wg.Go(func() {
 		for _, v := range tasks {
 			select {
-			// While we're trying to push some task in channel -> channel can be already blocked AND we can have many
-			// messages with errors
-			// So we just create priority select on writing.
 			case <-coordinatorCtx.Done():
 				close(taskChan)
 				return
-			default:
-				taskChan <- v
+			case taskChan <- v:
 			}
 		}
 		close(taskChan)
 	})
 }
 
-func ProcessTask(workerNumber int, t Task, errChan chan struct{}) {
+// ProcessTask is calling task with some wrapping.
+func ProcessTask(ctx context.Context, workerNumber int, t Task, successChan chan bool) {
+	success := true
 	err := t()
 	if err != nil {
 		fmt.Printf("Occure error in worker #%d: %v\n", workerNumber, err)
-		// think about blocking while writing. Is it safe? Maybe I need create some timeout or...?
-		errChan <- struct{}{}
-	} else {
-		fmt.Printf("Worker #%d completed task. Taking new\n", workerNumber)
+		success = false
 	}
+	fmt.Printf("Worker #%d completed task. Taking new\n", workerNumber)
+	select {
+	case successChan <- success:
+	case <-ctx.Done():
+	}
+}
+
+// AllTasksCounter represent struct for counting state of remain/all tasks.
+type AllTasksCounter struct {
+	all            int
+	errorsCounter  int
+	successCounter int
+}
+
+// GetAllSuccessCount provide count of all success completed task.
+func (t *AllTasksCounter) GetAllSuccessCount() int {
+	return t.successCounter
+}
+
+// GetAllErrorsCount provide of all success completed task.
+func (t *AllTasksCounter) GetAllErrorsCount() int {
+	return t.errorsCounter
+}
+
+// IncrementSuccessCounter increment count of completed tasks.
+func (t *AllTasksCounter) IncrementSuccessCounter() {
+	t.successCounter++
+}
+
+// IncrementErrorCounter increment count of tasks with errors.
+func (t *AllTasksCounter) IncrementErrorCounter() {
+	t.errorsCounter++
+}
+
+// GetAll provide us count of all given tasks.
+func (t *AllTasksCounter) GetAll() int {
+	return t.all
 }
